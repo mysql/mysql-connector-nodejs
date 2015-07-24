4d812531cf059f51a58aeb522f26c71e733bfce7
Johannes Schlüter <johannes.schlueter@oracle.com>
2015-07-24 22:57:01 +0200

    Change names to match dev api

--- test/Authentication/MySQL41Auth.js
+++ test/Authentication/MySQL41Auth.js
@@ -7,7 +7,7 @@ var username = "root";
 
 describe('MySQL41Auth', function () {
     it('should throw if the server sends less than 20 bytes', function () {
-        var auth = new MySQL41Auth({user: username, password: 'fff'});
+        var auth = new MySQL41Auth({dbUser: username, dbPassword: 'fff'});
         assert.throws(
             function () {
                 auth.getNextAuthData(new Buffer(19));
@@ -16,7 +16,7 @@ describe('MySQL41Auth', function () {
         );
     });
     it('should throw if the server sends more than 20 bytes', function () {
-        var auth = new MySQL41Auth({user: username, password: 'fff'});
+        var auth = new MySQL41Auth({dbUser: username, dbPassword: 'fff'});
         assert.throws(
             function () {
                 auth.getNextAuthData(new Buffer(22));
@@ -25,58 +25,58 @@ describe('MySQL41Auth', function () {
         );
     });
     it('should be long enough for 2*\\0 + username length + 1 yte for \'*\' +20 bytes for hash', function () {
-        var auth = new MySQL41Auth({user: username, password: 'fff'});
+        var auth = new MySQL41Auth({dbUser: username, dbPassword: 'fff'});
         var result = auth.getNextAuthData(new Buffer(20));
 
         assert.equal(result.length, 2 + username.length + 1 + 40);
     });
     it('should create a response starting with a \\0 byte', function () {
-        var auth = new MySQL41Auth({user: username, password: 'fff'});
+        var auth = new MySQL41Auth({dbUser: username, dbPassword: 'fff'});
         var result = auth.getNextAuthData(new Buffer(20));
 
         assert.equal(result[0], 0);
     });
     it('should return the username starting at offset 1 in the response', function () {
-        var auth = new MySQL41Auth({user: username, password: 'fff'});
+        var auth = new MySQL41Auth({dbUser: username, dbPassword: 'fff'});
         var result = auth.getNextAuthData(new Buffer(20));
 
         assert.equal(result.slice(1, username.length + 1).toString(), username);
     });
     it('should have a \\0 byte after username in the response', function () {
-        var auth = new MySQL41Auth({user: username, password: 'fff'});
+        var auth = new MySQL41Auth({dbUser: username, dbPassword: 'fff'});
         var result = auth.getNextAuthData(new Buffer(20));
 
         assert.equal(result[username.length + 1], 0);
     });
     it('should properly hash the password fff with a given hash', function () {
-        var auth = new MySQL41Auth({user: 'root', password: 'fff'});
+        var auth = new MySQL41Auth({dbUser: 'root', dbPassword: 'fff'});
         var result = auth.getNextAuthData(new Buffer([0xa, 0x35, 0x42, 0x1a, 0x43, 0x47, 0x6d, 0x65, 0x1, 0x4a, 0xf, 0x4c, 0x9, 0x5c, 0x32, 0x61, 0x64, 0x3c, 0x13, 0x6]));
 
         var exp = "*34439ed3004cf0e6030a9ec458338151bfb4e22d";
         assert.equal(result.slice(6).toString(), exp);
     });
     it('should return the properly hashed password with a given hash', function () {
-        var auth = new MySQL41Auth({user: username, password: 'fff'});
+        var auth = new MySQL41Auth({dbUser: username, dbPassword: 'fff'});
         var result = auth.getNextAuthData(new Buffer([0x41, 0x43, 0x56, 0x6e, 0x78, 0x19, 0x2c, 0x2c, 0x19, 0x6f, 0x18, 0x29, 0x05, 0x52, 0x3c, 0x62, 0x39, 0x3d, 0x5c, 0x77]));
 
         var exp = "*af1ef523d254181abb1155c1fbc933b80c2ec853";
         assert.equal(result.slice(6).toString(), exp);
     });
     it('should properly encode a result containing 0x10 and write "10" into the response buffer', function () {
-        var auth = new MySQL41Auth({user: username, password: 'fff'});
+        var auth = new MySQL41Auth({dbUser: username, dbPassword: 'fff'});
         var result = auth.getNextAuthData(new Buffer([0x7a, 0x59, 0x6b, 0x6e, 0x19, 0x7f, 0x44, 0x1, 0x6f, 0x4a, 0xf, 0xf, 0x3e, 0x19, 0x50, 0x4c, 0x4f, 0x47, 0x53, 0x5b]));
 
         var exp = "*950d944626109ab5bce8bc56a4e78a296e34271d";
         assert.equal(result.slice(6).toString(), exp);
     });
     it('should return a buffer of length = username + 2 if no password is given', function () {
-        var auth = new MySQL41Auth({user: username});
+        var auth = new MySQL41Auth({dbUser: username});
         var result = auth.getNextAuthData(new Buffer(20));
 
         assert.equal(result.length, username.length + 2);
     });
     it('should return a buffer of length = username + 3 if empty password is given', function () {
-        var auth = new MySQL41Auth({user: username, password: ''});
+        var auth = new MySQL41Auth({dbUser: username, dbPassword: ''});
         var result = auth.getNextAuthData(new Buffer(20));
 
         assert.equal(result.length, username.length + 2);
--- test/Authentication/PlainAuth.js
+++ test/Authentication/PlainAuth.js
@@ -8,7 +8,7 @@ var password = 'fff';
 
 describe('PlainAuth', function () {
     it('should throw if auth continue is requested', function () {
-        var auth = new PlainAuth({user: username, password: password});
+        var auth = new PlainAuth({dbUser: username, dbPassword: password});
         assert.throws(
             function () {
                 auth.getNextAuthData(new Buffer(20));
@@ -17,37 +17,37 @@ describe('PlainAuth', function () {
         );
     });
     it('password string should be long enough for 3*\\0 + username length + password length', function () {
-        var auth = new PlainAuth({user: username, password: password});
+        var auth = new PlainAuth({dbUser: username, dbPassword: password});
         var result = auth.getInitialAuthData();
 
         assert.equal(result.length, 3 + username.length + password.length);
     });
     it('password string should start with a \\0 byte', function () {
-        var auth = new PlainAuth({user: username, password: password});
+        var auth = new PlainAuth({dbUser: username, dbPassword: password});
         var result = auth.getInitialAuthData();
 
         assert.equal(result[0], 0);
     });
     it('password string should return the username starting at offset 1', function () {
-        var auth = new PlainAuth({user: username, password: password});
+        var auth = new PlainAuth({dbUser: username, dbPassword: password});
         var result = auth.getInitialAuthData();
 
         assert.equal(result.slice(1, username.length + 1).toString(), username);
     });
     it('password string should have a \\0 byte after username', function () {
-        var auth = new PlainAuth({user: username, password: password});
+        var auth = new PlainAuth({dbUser: username, dbPassword: password});
         var result = auth.getInitialAuthData();
 
         assert.equal(result[username.length + 1], 0);
     });
     it('password should return the password', function () {
-        var auth = new PlainAuth({user: username, password: password});
+        var auth = new PlainAuth({dbUser: username, dbPassword: password});
         var result = auth.getInitialAuthData();
 
         assert.equal(result.slice(username.length + 2, username.length + 2 + password.length).toString(), password);
     });
     it('password string should have a \\0 byte at the end', function () {
-        var auth = new PlainAuth({user: username, password: password});
+        var auth = new PlainAuth({dbUser: username, dbPassword: password});
         var result = auth.getInitialAuthData();
 
         assert.equal(result[username.length + 2 + password.length], 0);
