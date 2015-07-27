9855b4fdf1b73a15e3762ff25c1b921b059daedc
Johannes Schlüter <johannes.schlueter@oracle.com>
2015-07-27 19:15:29 +0200

    Some more tests and matching code fixes

--- test/Protocol/Datatype/encode.js
+++ test/Protocol/Datatype/encode.js
@@ -6,6 +6,16 @@ require('chai').should();
 describe('Data encoding', function () {
     [
         {
+            should: "encode NULL",
+            in: null,
+            exp: {
+                type: 1,
+                scalar: {
+                    type: 3
+                }
+            }
+        },
+        {
             should: "encode a simple string",
             in: "some string",
             exp: {
@@ -16,6 +26,68 @@ describe('Data encoding', function () {
                         value: "some string"
                     }
                 }
+            }
+        },
+        {
+            should: "encode a string with \\0 byte",
+            in: "some\0string",
+            exp: {
+                type: 1,
+                scalar: {
+                    type: 8,
+                    v_string: {
+                        value: "some\0string"
+                    }
+                }
+            }
+        },
+        {
+            should: "encode a string with Umlauts",
+            in: "some äöüß string",
+            exp: {
+                type: 1,
+                scalar: {
+                    type: 8,
+                    v_string: {
+                        value: "some äöüß string"
+                    }
+                }
+            }
+        },
+        {
+            should: "encode an array of string",
+            in: [ "aaa", "bbb", "ccc"],
+            exp: {
+                type: 3,
+                array: [
+                    {
+                        type: 1,
+                        scalar: {
+                            type: 8,
+                            v_string: {
+                                value: "aaa"
+                            }
+                        }
+                    },
+                    {
+                        type: 1,
+                        scalar: {
+                            type: 8,
+                            v_string: {
+                                value: "bbb"
+                            }
+                        }
+                    },
+                    {
+                        type: 1,
+                        scalar: {
+                            type: 8,
+                            v_string: {
+                                value: "ccc"
+                            }
+                        }
+                    }
+                ]
             }
         }
     ].forEach(function (input) {
