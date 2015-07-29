bf763a1bc8c42b86037bbfbc597f4c7180507fea
Johannes Schlüter <johannes.schlueter@oracle.com>
2015-07-30 01:14:20 +0200

    Add a few parser tests (one failing)

--- test/Expressions/parse.js
+++ test/Expressions/parse.js
@@ -23,7 +23,6 @@ describe('MySQLx Expression parsing', function () {
                         {
                             type: 1,
                             identifier: {
-                                /*name: "doc",*/
                                 document_path: {
                                     type: 1,
                                     value: "name"
@@ -33,6 +32,92 @@ describe('MySQLx Expression parsing', function () {
                         {
                             type: 2,
                             constant: Datatype.encode("Johannes")
+                        }
+                    ]
+                }
+            }
+        },
+        {
+            should: 'allow functions without arguments',
+            in: 'concat()',
+            exp: {
+                type: 4,
+                function_call: {
+                    name: {
+                        name: "concat"
+                    },
+                    param: []
+                }
+            }
+        },
+        {
+            should: 'allow functions with single argument',
+            in: 'concat("foo")',
+            exp: {
+                type: 4,
+                function_call: {
+                    name: {
+                        name: "concat"
+                    },
+                    param: [
+                        {
+                            type: 2,
+                            constant: Datatype.encode("foo")
+                        }
+                    ]
+                }
+            }
+        },
+        {
+            should: 'allow functions with multiple arguments',
+            in: 'concat("foo", "bar")',
+            exp: {
+                type: 4,
+                function_call: {
+                    name: {
+                        name: "concat"
+                    },
+                    param: [
+                        {
+                            type: 2,
+                            constant: Datatype.encode("foo")
+                        },
+                        {
+                            type: 2,
+                            constant: Datatype.encode("bar")
+                        }
+                    ]
+                }
+            }
+        },
+        {
+            should: 'allow nested function calls',
+            in: 'concat("foo", concat("bar"))',
+            exp: {
+                type: 4,
+                function_call: {
+                    name: {
+                        name: "concat"
+                    },
+                    param: [
+                        {
+                            type: 2,
+                            constant: Datatype.encode("foo")
+                        },
+                        {
+                            type: 4,
+                            function_call: {
+                                name: {
+                                    name: "concat"
+                                },
+                                param: [
+                                    {
+                                        type: 2,
+                                        constant: Datatype.encode("bar")
+                                    }
+                                ]
+                            }
+
                         }
                     ]
                 }
