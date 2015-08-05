80d2b2f1fa04cd83391f83d098a7b4cf51731a04
Johannes Schlüter <johannes.schlueter@oracle.com>
2015-08-05 18:51:11 +0200

    Simplify  tests with beforeEach etc.

--- test/DevAPI/CollectionAdd.js
+++ test/DevAPI/CollectionAdd.js
@@ -9,68 +9,69 @@ var NullAuth = require('../../lib/Authentication/NullAuth');
 
 chai.use(spies);
 
-var nullStream = {
-    on: function () {},
-    write: function () {}
-};
-
-var NullStreamFactory = {
-    ceateSocket: function () {
-        return nullStream;
-    }
-};
-
-function getTestSession() {
-    return mysqlx.getSession({
-        auth_method: "NullAuth",
-        socket_factory: NullStreamFactory
-    });
-}
 
 describe('DevAPI Collection Add', function () {
-    it('should request protocol to add one item', function () {
-        var spy = chai.spy(Protocol.prototype.crudInsert);
+    var collection, spy, origInsert;
+
+    var nullStream = {
+        on: function () {},
+        write: function () {}
+    };
+
+    var NullStreamFactory = {
+        createSocket: function () {
+            return new Promise(function (resolve) {
+                resolve(nullStream);
+            });
+        }
+    };
+
+    beforeEach('get Session', function (done) {
+        mysqlx.getSession({
+            auth_method: "NULL",
+            socket_factory: NullStreamFactory
+        }).then(function (session) {
+            collection = session.getSchema("schema").getCollection("collection");
+            done();
+        }).catch(function (err) {
+            done(err);
+        });
+    });
+
+    beforeEach('create spy', function () {
+        spy = chai.spy(Protocol.prototype.crudInsert);
+        origInsert = Protocol.prototype.crudInsert;
         Protocol.prototype.crudInsert = spy;
+    });
 
-        return getTestSession().then(function (session) {
-            session.getSchema("schema").getCollection("collection").add({_id: 12}).execute();
+    afterEach('reset spy', function () {
+        Protocol.prototype.crudInsert = origInsert;
+    });
 
-            spy.should.be.called.once.with("schema", "collection", [{_id: 12}]);
 
-            return true;
-        }).should.be.fullfilled;
+    it('should request protocol to add one item', function () {
+        collection.add({_id: 12}).execute();
+        spy.should.be.called.once.with("schema", "collection", [{_id: 12}]);
+    });
+
+    it('should request protocol to add one item', function () {
+        collection.add({_id: 12}).execute();
+        spy.should.be.called.once.with("schema", "collection", [{_id: 12}]);
     });
     it('should request protocol to add an array of items', function () {
-        var spy = chai.spy(Protocol.prototype.crudInsert);
-        Protocol.prototype.crudInsert = spy;
-
-        return getTestSession().then(function (session) {
-            session.getSchema("schema").getCollection("collection").add([{_id: 12}, {_id: 34}]).execute();
-
-            spy.should.be.called.once.with("schema", "collection", [{_id: 12}, {_id: 34}]);
-        }).should.be.fullfilled;
+        collection.add([{_id: 12}, {_id: 34}]).execute();
+        spy.should.be.called.once.with("schema", "collection", [{_id: 12}, {_id: 34}]);
     });
     it('should request protocol to add items passed via varargs', function () {
-        var spy = chai.spy(Protocol.prototype.crudInsert);
-        Protocol.prototype.crudInsert = spy;
-
-        return getTestSession().then(function (session) {
-            session.getSchema("schema").getCollection("collection").add({_id: 12}, {_id: 34}).execute();
-
-            spy.should.be.called.once.with("schema", "collection", [{_id: 12}, {_id: 34}]);
-        }).should.be.fullfilled;
+        collection.add({_id: 12}, {_id: 34}).execute();
+        spy.should.be.called.once.with("schema", "collection", [{_id: 12}, {_id: 34}]);
     });
     it('should create an _id field if none was provided', function () {
-        var spy = chai.spy(Protocol.prototype.crudInsert);
-        Protocol.prototype.crudInsert = spy;
-
         var doc = { foo: 12 };
 
-        return getTestSession().then(function (session) {
-            session.getSchema("schema").getCollection("collection").add(doc).execute();
+        collection.add(doc).execute();
 
-            spy.should.be.called.once();
-            should.exist(doc._id);
-        }).should.be.fullfilled;
+        spy.should.be.called.once();
+        should.exist(doc._id);
     });
 });
