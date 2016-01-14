e6a84ab08fc61de793dc79eef96a6e12ae245e5c
Johannes Schlüter <johannes.schlueter@oracle.com>
2016-01-14 22:11:36 +0100

    Add a few tests for capabilitiesSet

---
+++ test/Protocol/capabilitiesSet.js
@@ -0,0 +1,90 @@
+"use strict";
+
+var chai = require("chai");
+chai.should();
+
+var assert = require("assert");
+var Protocol = require("../../lib/Protocol");
+var Datatype = require("../../lib/Protocol/Datatype");
+var Messages = require('../../lib/Protocol/Messages');
+
+var nullStream = {
+    on: function () {},
+    write: function () {}
+};
+
+describe('Protocol', function () {
+    describe('capabilitiesSet', function () {
+        it('should send a Capabilities Set message', function () {
+            var sentData = null;
+
+            var mockedStream = {
+                on: function () {
+                },
+                write: function (data) {
+                    sentData = data;
+                }
+            };
+
+            var protocol = new Protocol(mockedStream);
+            assert.strictEqual(sentData, null, "There was data sent too early");
+            protocol.capabilitiesSet({});
+            assert.notEqual(sentData, null, "There was no data sent");
+            var data = protocol.decodeMessage(sentData, 0, protocol.clientMessages);
+            assert.strictEqual(data.messageId, Messages.ClientMessages.CON_CAPABILITIES_SET);
+        });
+        it('should send a Capabilities Set message with a single value', function () {
+            var sentData = null;
+
+            var mockedStream = {
+                on: function () {
+                },
+                write: function (data) {
+                    sentData = data;
+                }
+            };
+
+            var protocol = new Protocol(mockedStream);
+            assert.strictEqual(sentData, null, "There was data sent too early");
+            protocol.capabilitiesSet({ option: 42 });
+            assert.notEqual(sentData, null, "There was no data sent");
+            var data = protocol.decodeMessage(sentData, 0, protocol.clientMessages);
+            assert.strictEqual(data.messageId, Messages.ClientMessages.CON_CAPABILITIES_SET);
+            assert.strictEqual(data.decoded.capabilities.capabilities.length, 1);
+            assert.strictEqual(data.decoded.capabilities.capabilities[0].name, 'option');
+        });
+        it('should send a Capabilities Set message with multiple values', function () {
+            var sentData = null;
+
+            var mockedStream = {
+                on: function () {
+                },
+                write: function (data) {
+                    sentData = data;
+                }
+            };
+
+            var protocol = new Protocol(mockedStream);
+            assert.strictEqual(sentData, null, "There was data sent too early");
+            protocol.capabilitiesSet({ option1: 42, option2: 24 });
+            assert.notEqual(sentData, null, "There was no data sent");
+            var data = protocol.decodeMessage(sentData, 0, protocol.clientMessages);
+            assert.strictEqual(data.messageId, Messages.ClientMessages.CON_CAPABILITIES_SET);
+            assert.strictEqual(data.decoded.capabilities.capabilities.length, 2);
+            assert.strictEqual(data.decoded.capabilities.capabilities[0].name, 'option1');
+            assert.strictEqual(data.decoded.capabilities.capabilities[1].name, 'option2');
+        });
+        it('should resolve Promise', function () {
+            var protocol = new Protocol(nullStream);
+            var promise = protocol.capabilitiesSet({});
+            protocol.handleServerMessage(protocol.encodeMessage(Messages.ServerMessages.OK, {}, protocol.serverMessages));
+            return promise.should.be.fulfilled;
+        });
+        it('should fail on error', function () {
+            var protocol = new Protocol(nullStream);
+            var promise = protocol.capabilitiesSet({});
+            protocol.handleServerMessage(protocol.encodeMessage(Messages.ServerMessages.ERROR, { code: 1, sql_state: 'HY000', msg: 'Invalid'}, protocol.serverMessages));
+            return promise.should.be.rejected;
+        });
+    });
+});
