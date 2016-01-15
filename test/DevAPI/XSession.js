8efb4893574653dccf1ca2f6c6bb71097961cf55
Johannes Schlüter <johannes.schlueter@oracle.com>
2016-01-15 02:36:42 +0100

    Ad a few more tests

--- test/DevAPI/XSession.js
+++ test/DevAPI/XSession.js
@@ -1,6 +1,14 @@
+"use strict";
+
+var chai = require('chai'),
+    should = chai.should(),
+    spies = require('chai-spies');
 var expect = require('chai').expect;
-var Session = require('../../lib/DevAPI/BaseSession');
-var Schema = require('../../lib/DevAPI/Schema');
+var mysqlx = require('../../'),
+    Session = require('../../lib/DevAPI/BaseSession'),
+    Schema = require('../../lib/DevAPI/Schema'),
+    Protocol = require('../../lib/Protocol'),
+    NullAuth = require('../../lib/Authentication/NullAuth');
 
 describe('XSession', function () {
     it('should have a getSchema method', function () {
@@ -11,4 +19,57 @@ describe('XSession', function () {
         var session = new Session({});
         expect(session.getSchema()).to.exist.and.be.instanceof(Schema);
     });
+    describe('Tests using executing SQL', function() {
+        var session, spy, origExecSql;
+
+        var nullStream = {
+            on: function () {},
+            write: function () {}
+        };
+
+        var NullStreamFactory = {
+            createSocket: function () {
+                return new Promise(function (resolve) {
+                    resolve(nullStream);
+                });
+            }
+        };
+
+        beforeEach('get Session', function (done) {
+            return mysqlx.getSession({
+                authMethod: "NULL",
+                socketFactory: NullStreamFactory
+            }).then(function (s) {
+                session = s;
+                done();
+            }).catch(function (err) {
+                done(err);
+            });
+        });
+
+        beforeEach('create spy', function () {
+            spy = chai.spy(Protocol.prototype.sqlStmtExecute);
+            origExecSql = Protocol.prototype.sqlStmtExecute;
+            Protocol.prototype.sqlStmtExecute = spy;
+        });
+
+        afterEach('reset spy', function () {
+            if (origExecSql) {
+                Protocol.prototype.sqlStmtExecute = origExecSql;
+            }
+        });
+
+        it('should query the server for schemas', function () {
+            session.getSchemas().should.be.an.instanceof(Promise);
+            spy.should.be.called.once;
+        });
+        it('should tell server to create a schema', function () {
+            session.createSchema("foo").should.be.an.instanceof(Promise);
+            spy.should.be.called.once;
+        });
+        it('should tell server to drop a schema', function () {
+            session.dropSchema("foo").should.be.an.instanceof(Promise);
+            spy.should.be.called.once;
+        });
+    })
 });
