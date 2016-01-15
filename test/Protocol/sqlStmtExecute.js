8f3d9d605167f0164b894f886a71ab7692a3dc31
Johannes Schlüter <johannes.schlueter@oracle.com>
2016-01-15 17:21:41 +0100

    A few more SQL resul tests

--- test/Protocol/sqlStmtExecute.js
+++ test/Protocol/sqlStmtExecute.js
@@ -15,7 +15,7 @@ var nullStream = {
     write: function () {}
 };
 
-function produceResultSet(protocol, columnCount) {
+function produceResultSet(protocol, columnCount, rowCount) {
     for (let i = 0; i < columnCount; ++i) {
         protocol.handleServerMessage(protocol.encodeMessage(Messages.ServerMessages.RESULTSET_COLUMN_META_DATA, {
             type: Messages.messages['Mysqlx.Resultset.ColumnMetaData'].enums.FieldType.SINT,
@@ -26,22 +26,61 @@ function produceResultSet(protocol, columnCount) {
             schema: "schema"
         }, protocol.serverMessages));
     }
-    //protocol.handleServerMessage(protocol.encodeMessage(Messages.ServerMessages.RESULTSET_ROW, {}, protocol.serverMessages));
+    for (let r = 0; r < rowCount; ++r) {
+        let fields = [];
+        for (let c = 0; c < columnCount; ++c) {
+            fields.push("\x01");
+        }
+        protocol.handleServerMessage(protocol.encodeMessage(Messages.ServerMessages.RESULTSET_ROW, { field: fields }, protocol.serverMessages));
+    }
     protocol.handleServerMessage(protocol.encodeMessage(Messages.ServerMessages.RESULTSET_FETCH_DONE, {}, protocol.serverMessages));
     protocol.handleServerMessage(protocol.encodeMessage(Messages.ServerMessages.SQL_STMT_EXECUTE_OK, {}, protocol.serverMessages));
 }
 
 describe('Protocol', function () {
-    describe('sqlStagementExecute', function () {
+    describe('sqlStatementExecute', function () {
+        it('should throw if row callback is no function', function () {
+            const protocol = new Protocol(nullStream);
+            (() => {
+                protocol.sqlStmtExecute("invalid SQL", [], "this is not a function");
+            }).should.throw(/.*has to be a function.*/);
+        });
+        it('should throw if meta callback is no function', function () {
+            const protocol = new Protocol(nullStream);
+            (() => {
+                protocol.sqlStmtExecute("invalid SQL", [], ()=>{}, "this is not a function");
+            }).should.throw(/.*has to be a function.*/);
+        });
+        it('should fail the promise on error', function () {
+            const protocol = new Protocol(nullStream),
+                  promise = protocol.sqlStmtExecute("invalid SQL", [], () => {});
+
+            protocol.handleServerMessage(protocol.encodeMessage(Messages.ServerMessages.ERROR, {
+                code: 1064,
+                sql_state: "42000",
+                msg: 'You have an error in your SQL syntax'
+            }, protocol.serverMessages));
+            return promise.should.be.rejected;
+        });
         it('should report on meta data', function () {
-            var protocol = new Protocol(nullStream);
+            const protocol = new Protocol(nullStream),
+                  metacb = chai.spy(),
+                  promise = protocol.sqlStmtExecute("SELECT * FROM t", [], () => {}, metacb);
 
-            var metacb = chai.spy();
-
-            var promise = protocol.sqlStmtExecute("SELECT * FROM t", [], row => {}, metacb);
-            produceResultSet(protocol, 1);
+            produceResultSet(protocol, 1, 1);
             metacb.should.have.been.called.once;
             return promise.should.be.fulfilled;
         });
+        for (let count = 0; count < 3; ++count) {
+            it('should call row callback for each row data ('+count+' rows)', function () {
+                const protocol = new Protocol(nullStream),
+                      rowcb = chai.spy(),
+                      promise = protocol.sqlStmtExecute("SELECT * FROM t", [], rowcb);
+
+                produceResultSet(protocol, 1, count);
+                rowcb.should.have.been.called.exactly(count);
+                return promise.should.be.fulfilled;
+            });
+        }
     });
 });
