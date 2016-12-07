58e4858bf38436189fb669c7c561a4e9c51d1fce
Rui Quelhas <rui.quelhas@oracle.com>
2016-12-07 15:04:51 +0000

    Add support for IPv6 connections

--- test/integration/session.js
+++ test/integration/session.js
@@ -2,37 +2,83 @@
 
 /* eslint-env node, mocha */
 
-const mysqlx = require('index');
+const BaseSession = require('lib/DevAPI/BaseSession');
 const config = require('./properties');
+const expect = require('chai').expect;
+const mysqlx = require('index');
 
 describe('@integration server connection', () => {
     context('single server running the X plugin', () => {
-        it('should connect to the server in a new session', () => {
-            return mysqlx.getNodeSession(config).should.be.fulfilled;
-        });
-
-        it('should not connect if the credentials are invalid', () => {
-            const invalidConfig = Object.assign({}, config, {
-                dbUser: 'invalid user',
-                dbPassword: 'invalid password'
+        context('using a configuration object', () => {
+            it('should connect to the server in a new session', () => {
+                return mysqlx.getNodeSession(config).should.be.fulfilled;
             });
 
-            return mysqlx.getNodeSession(invalidConfig).should.be.rejected;
+            it('should connect to the server with an IPv6 host', () => {
+                const ipv6Config = Object.assign({}, config, { host: '::1' });
+
+                return mysqlx.getNodeSession(ipv6Config).should.be.fulfilled;
+            });
+
+            it('should not connect if the credentials are invalid', () => {
+                const invalidConfig = Object.assign({}, config, {
+                    dbUser: 'invalid user',
+                    dbPassword: 'invalid password'
+                });
+
+                return mysqlx.getNodeSession(invalidConfig).should.be.rejected;
+            });
+        });
+
+        context('using a RFC-3986 URI', () => {
+            it('should connect to the server with an IPv6 host', () => {
+                const ipv6Config = Object.assign({}, config, { host: '::1' });
+                // TODO(rui.quelhas): use ES6 destructuring assignment for node >=6.0.0
+                const uri = `mysqlx://${ipv6Config.dbUser}:${ipv6Config.dbPassword}@[${ipv6Config.host}]:${ipv6Config.port}`;
+                const expected = { dbUser: ipv6Config.dbUser, host: ipv6Config.host, port: ipv6Config.port };
+
+                return mysqlx.getNodeSession(uri).then(result => {
+                    expect(result).to.be.an.instanceof(BaseSession);
+                    expect(result.inspect()).to.deep.equal(expected);
+                });
+            });
         });
     });
 
     context('server cluster abstraction', () => {
-        it('should connect to the server in a new session', () => {
-            return mysqlx.getSession(config).should.be.fulfilled;
-        });
-
-        it('should not connect if the credentials are invalid', () => {
-            const invalidConfig = Object.assign({}, config, {
-                dbUser: 'invalid user',
-                dbPassword: 'invalid password'
+        context('using a configuration object', () => {
+            it('should connect to the server in a new session', () => {
+                return mysqlx.getSession(config).should.be.fulfilled;
             });
 
-            return mysqlx.getSession(invalidConfig).should.be.rejected;
+            it('should connect to the server with an IPv6 host', () => {
+                const ipv6Config = Object.assign({}, config, { host: '::1' });
+
+                return mysqlx.getSession(ipv6Config).should.be.fulfilled;
+            });
+
+            it('should not connect if the credentials are invalid', () => {
+                const invalidConfig = Object.assign({}, config, {
+                    dbUser: 'invalid user',
+                    dbPassword: 'invalid password'
+                });
+
+                return mysqlx.getSession(invalidConfig).should.be.rejected;
+            });
+        });
+
+        context('using a RFC-3986 URI', () => {
+            it('should connect to the server with an IPv6 host', () => {
+                const ipv6Config = Object.assign({}, config, { host: '::1' });
+                // TODO(rui.quelhas): use ES6 destructuring assignment for node >=6.0.0
+                const uri = `mysqlx://${ipv6Config.dbUser}:${ipv6Config.dbPassword}@[${ipv6Config.host}]:${ipv6Config.port}`;
+                const expected = { dbUser: ipv6Config.dbUser, host: ipv6Config.host, port: ipv6Config.port };
+
+                return mysqlx.getSession(uri).then(result => {
+                    expect(result).to.be.an.instanceof(BaseSession);
+                    expect(result.inspect()).to.deep.equal(expected);
+                });
+            });
         });
     });
 });
--- test/unit/DevAPI/Util/parseUri.js
+++ test/unit/DevAPI/Util/parseUri.js
@@ -1,10 +1,11 @@
-"use strict";
+'use strict';
 
-const parseUri = require('../../../../lib/DevAPI/Util/parseUri');
-require('chai').should();
+/* eslint-env node, mocha */
 
+const parseUri = require('lib/DevAPI/Util/parseUri');
+const expect = require('chai').expect;
 
-describe('MySQLx URL parsing', function () {
+describe('MySQLx URL parsing', () => {
     [
         {
             should: 'parse a full URL',
@@ -125,10 +126,101 @@ describe('MySQLx URL parsing', function () {
                 host: 'hostname',
                 port: '3357'
             }
+        },
+        {
+            should: 'parse IPv6 loopback host, no password',
+            in: 'mysqlx://root@[::]:33060',
+            exp: {
+                dbUser: 'root',
+                dbPassword: '',
+                host: '::',
+                port: '33060'
+            }
+        },
+        {
+            should: 'parse an arbitrary IPv6 host',
+            in: 'mysqlx://root:s3ce3t!@[a1:a2:a3:a4:a5:a6:a7:a8]:3306',
+            exp: {
+                dbUser: 'root',
+                dbPassword: 's3ce3t!',
+                host: 'a1:a2:a3:a4:a5:a6:a7:a8',
+                port: '3306'
+            }
+        },
+        {
+            should: 'parse an URI in the old format',
+            in: 'root:password@hostname:3357',
+            exp: {
+                dbUser: 'root',
+                dbPassword: 'password',
+                host: 'hostname',
+                port: '3357'
+            }
+        },
+        {
+            should: 'parse an URI in the old format, no password',
+            in: 'root@hostname:3357',
+            exp: {
+                dbUser: 'root',
+                dbPassword: '',
+                host: 'hostname',
+                port: '3357'
+            }
+        },
+        {
+            should: 'parse an URI in the old format, empty password',
+            in: 'root:@hostname:3357',
+            exp: {
+                dbUser: 'root',
+                dbPassword: '',
+                host: 'hostname',
+                port: '3357'
+            }
+        },
+        {
+            should: 'parse an URI in the old format, no port',
+            in: 'root:password@hostname',
+            exp: {
+                dbUser: 'root',
+                dbPassword: 'password',
+                host: 'hostname',
+                port: undefined
+            }
+        },
+        {
+            should: 'parse an URI in the old format, empty port',
+            in: 'root:password@hostname',
+            exp: {
+                dbUser: 'root',
+                dbPassword: 'password',
+                host: 'hostname',
+                port: undefined
+            }
+        },
+        {
+            should: 'parse an IPv6 host URI in the old format',
+            in: 'root:password@[a1:a2:a3:a4:a5:a6:a7:a8]:3357',
+            exp: {
+                dbUser: 'root',
+                dbPassword: 'password',
+                host: 'a1:a2:a3:a4:a5:a6:a7:a8',
+                port: '3357'
+            }
         }
-    ].forEach(function (expression) {
-        it('should ' + expression.should + ' (' + expression.in + ')', function () {
-            parseUri(expression.in).should.deep.equal(expression.exp);
+    ].forEach(expression => {
+        it('should ' + expression.should + ' (' + expression.in + ')', () => {
+            expect(parseUri(expression.in)).to.deep.equal(expression.exp);
+        });
+    });
+
+    it('should throw an error for an invalid old-format URI', () => {
+        [
+            'root',
+            'root:password',
+            'root:password@',
+            'root:password@:3357'
+        ].forEach(invalid => {
+            expect(() => parseUri(invalid)).to.throw(Error, 'Invalid URI');
         });
     });
 });
