4ba5df048bc2d747341d75b76634eeede3dc2f87
Rui Quelhas <rui.quelhas@oracle.com>
2016-12-19 16:22:07 +0000

    Add URI/connection string schema parsing support

--- test/unit/DevAPI/Util/parseUri.js
+++ test/unit/DevAPI/Util/parseUri.js
@@ -6,56 +6,8 @@ const parseUri = require('lib/DevAPI/Util/parseUri');
 const expect = require('chai').expect;
 
 describe('MySQLx URL parsing', () => {
-    it('should parse an URI with a hostname', () => {
-        const expected = {
-            dbUser: 'user',
-            dbPassword: 'password',
-            host: 'hostname',
-            port: '3357'
-        };
-
-        expect(parseUri('mysqlx://user:password@hostname:3357')).to.deep.equal(expected);
-    });
-
-    it('should parse an URI with an arbitrary IPv4 host', () => {
-        const expected = {
-            dbUser: 'user',
-            dbPassword: 'password',
-            host: '127.0.0.1',
-            port: '3357'
-        };
-
-        expect(parseUri('mysqlx://user:password@127.0.0.1:3357')).to.deep.equal(expected);
-    });
-
-    it('should parse an URI with a loopback IPv6 host', () => {
-        const expected = {
-            dbUser: 'user',
-            dbPassword: 'password',
-            host: '::',
-            port: '3357'
-        };
-
-        expect(parseUri('mysqlx://user:password@[::]:3357')).to.deep.equal(expected);
-    });
-
-    it('should parse an URI with an arbitrary IPv6 host', () => {
-        const expected = {
-            dbUser: 'user',
-            dbPassword: 'password',
-            host: 'a1:a2:a3:a4:a5:a6:a7:a8',
-            port: '3357'
-        };
-
-        expect(parseUri('mysqlx://user:password@[a1:a2:a3:a4:a5:a6:a7:a8]:3357')).to.deep.equal(expected);
-    });
-
-    it('should throw an error if the host is not provided', () => {
-        expect(() => parseUri('mysqlx://user:password')).to.throw(Error);
-    });
-
-    context('for an old-format (without scheme)', () => {
-        it('should parse a complete URI', () => {
+    context('for RFC-3986 URIs', () => {
+        it('should parse an URI with a hostname', () => {
             const expected = {
                 dbUser: 'user',
                 dbPassword: 'password',
@@ -63,10 +15,72 @@ describe('MySQLx URL parsing', () => {
                 port: '3357'
             };
 
-            expect(parseUri('user:password@hostname:3357')).to.deep.equal(expected);
+            expect(parseUri('mysqlx://user:password@hostname:3357')).to.deep.equal(expected);
         });
 
-        it('should parse a URI if the password is not provided', () => {
+        it('should parse an URI with an arbitrary IPv4 host', () => {
+            const expected = {
+                dbUser: 'user',
+                dbPassword: 'password',
+                host: '127.0.0.1',
+                port: '3357'
+            };
+
+            expect(parseUri('mysqlx://user:password@127.0.0.1:3357')).to.deep.equal(expected);
+        });
+
+        it('should parse an URI with a loopback IPv6 host', () => {
+            const expected = {
+                dbUser: 'user',
+                dbPassword: 'password',
+                host: '::',
+                port: '3357'
+            };
+
+            expect(parseUri('mysqlx://user:password@[::]:3357')).to.deep.equal(expected);
+        });
+
+        it('should parse an URI with an arbitrary IPv6 host', () => {
+            const expected = {
+                dbUser: 'user',
+                dbPassword: 'password',
+                host: 'a1:a2:a3:a4:a5:a6:a7:a8',
+                port: '3357'
+            };
+
+            expect(parseUri('mysqlx://user:password@[a1:a2:a3:a4:a5:a6:a7:a8]:3357')).to.deep.equal(expected);
+        });
+
+        it('should parse an URI with an empty path', () => {
+            const expected = {
+                dbUser: 'user',
+                dbPassword: 'password',
+                host: 'hostname',
+                port: '3357'
+            };
+
+            expect(parseUri('mysqlx://user:password@hostname:3357/')).to.deep.equal(expected);
+        });
+
+        it('should parse a complete URI', () => {
+            const expected = {
+                dbUser: 'user',
+                dbPassword: 'password',
+                host: 'hostname',
+                port: '3357',
+                schema: 'schema'
+            };
+
+            expect(parseUri('mysqlx://user:password@hostname:3357/schema')).to.deep.equal(expected);
+        });
+
+        it('should throw an error if the host is not provided', () => {
+            expect(() => parseUri('mysqlx://')).to.throw(Error);
+        });
+    });
+
+    context('for a unified connection string', () => {
+        it('should parse a connection string if the password is not provided', () => {
             const expected = {
                 dbUser: 'user',
                 dbPassword: '',
@@ -77,7 +91,7 @@ describe('MySQLx URL parsing', () => {
             expect(parseUri('user@hostname:3357')).to.deep.equal(expected);
         });
 
-        it('should parse a URI if the password is empty', () => {
+        it('should parse a connection string if the password is empty', () => {
             const expected = {
                 dbUser: 'user',
                 dbPassword: '',
@@ -88,7 +102,7 @@ describe('MySQLx URL parsing', () => {
             expect(parseUri('user:@hostname:3357')).to.deep.equal(expected);
         });
 
-        it('should parse a URI if the port is not provided', () => {
+        it('should parse a connection string if the port is not provided', () => {
             const expected = {
                 dbUser: 'user',
                 dbPassword: '',
@@ -99,7 +113,7 @@ describe('MySQLx URL parsing', () => {
             expect(parseUri('user@hostname')).to.deep.equal(expected);
         });
 
-        it('should parse a URI if the port is empty', () => {
+        it('should parse a connection string if the port is empty', () => {
             const expected = {
                 dbUser: 'user',
                 dbPassword: '',
