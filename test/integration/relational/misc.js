384dff71656781b4411aa7f6268a0bd59d99e103
Rui Quelhas <rui.quelhas@oracle.com>
2016-12-26 14:20:15 +0000

    Relocate `NodeSession` integration tests

---
+++ test/integration/relational/misc.js
@@ -0,0 +1,94 @@
+'use strict';
+
+/* eslint-env node, mocha */
+
+const Column = require('lib/DevAPI/Column');
+const config = require('test/integration/properties');
+const expect = require('chai').expect;
+const fixtures = require('test/integration/fixtures');
+const mysqlx = require('index');
+
+// TODO(rui.quelhas): extract tests into proper self-contained suites.
+describe('@integration relational miscellaneous tests', () => {
+    let session;
+
+    beforeEach('set context', () => {
+        return mysqlx.getNodeSession(config).then(s => {
+            // TODO(rui.quelhas): use ES6 destructuring assignment for node >=6.0.0
+            session = s;
+        });
+    });
+
+    afterEach('clear context', () => {
+        return fixtures.teardown(session);
+    });
+
+    context('raw SQL query', () => {
+        it('should handle the results with a callback provided as an argument', () => {
+            const expected = [1];
+            let actual;
+
+            return session
+                .executeSql('SELECT 1')
+                .execute(row => { actual = row; })
+                .then(() => expect(actual).to.deep.equal(expected));
+        });
+
+        it('should handle the column metadata with a callback provided as an argument', () => {
+            const expected = {
+                meta: [new Column({
+                    type: 1,
+                    name: '1',
+                    original_name: '',
+                    table: '',
+                    original_table: '',
+                    schema: '',
+                    catalog: 'def',
+                    collation: '0',
+                    fractional_digits: 0,
+                    length: 1,
+                    flags: 16
+                })],
+                row: [1]
+            };
+            let actual = {};
+
+            return session
+                .executeSql('SELECT 1')
+                .execute(row => { actual.row = row; }, meta => { actual.meta = meta; })
+                .then(() => expect(actual).to.deep.equal(expected));
+        });
+
+        it('should handle the results using both callbacks provided in an object', () => {
+            const expected = {
+                meta: [new Column({
+                    type: 1,
+                    name: '1',
+                    original_name: '',
+                    table: '',
+                    original_table: '',
+                    schema: '',
+                    catalog: 'def',
+                    collation: '0',
+                    fractional_digits: 0,
+                    length: 1,
+                    flags: 16
+                })],
+                row: [1]
+            };
+            let actual = {};
+
+            return session
+                .executeSql('SELECT 1')
+                .execute({
+                    row (row) {
+                        actual.row = row;
+                    },
+                    meta (meta) {
+                        actual.meta = meta;
+                    }
+                })
+                .then(() => expect(actual).to.deep.equal(expected));
+        });
+    });
+});
