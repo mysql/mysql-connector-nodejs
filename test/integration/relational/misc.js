f36e6bb3a7f503a9d2c092b6e3cc6032bb5c9eb8
Rui Quelhas <rui.quelhas@oracle.com>
2017-11-16 17:03:14 +0000

    Fix 8.0.4-rc breaking changes in the test suite

--- test/integration/relational/misc.js
+++ test/integration/relational/misc.js
@@ -34,48 +34,27 @@ describe('@integration relational miscellaneous tests', () => {
         });
 
         it('should handle the column metadata with a callback provided as an argument', () => {
-            const expected = {
-                meta: [new Column({
-                    type: 1,
-                    name: '1',
-                    original_name: '',
-                    table: '',
-                    original_table: '',
-                    schema: '',
-                    catalog: 'def',
-                    collation: '0',
-                    fractional_digits: 0,
-                    length: 1,
-                    flags: 16
-                })],
-                row: [1]
-            };
-            let actual = {};
+            const actual = {};
 
             return session
                 .executeSql('SELECT 1')
                 .execute(row => { actual.row = row; }, meta => { actual.meta = meta; })
-                .then(() => expect(actual).to.deep.equal(expected));
+                .then(() => {
+                    expect(actual).to.have.all.keys('meta', 'row');
+                    expect(actual.meta).to.have.lengthOf(1);
+                    expect(actual.meta[0]).to.be.an.instanceOf(Column);
+                    expect(actual.meta[0].getType()).to.equal(1);
+                    expect(actual.meta[0].getColumnLabel()).to.equal('1');
+                    expect(actual.meta[0].getColumnName()).to.equal('');
+                    expect(actual.meta[0].getTableLabel()).to.equal('');
+                    expect(actual.meta[0].getTableName()).to.equal('');
+                    expect(actual.meta[0].getSchemaName()).to.equal('');
+                    expect(actual.meta[0].getLength()).to.equal(1);
+                });
         });
 
         it('should handle the results using both callbacks provided in an object', () => {
-            const expected = {
-                meta: [new Column({
-                    type: 1,
-                    name: '1',
-                    original_name: '',
-                    table: '',
-                    original_table: '',
-                    schema: '',
-                    catalog: 'def',
-                    collation: '0',
-                    fractional_digits: 0,
-                    length: 1,
-                    flags: 16
-                })],
-                row: [1]
-            };
-            let actual = {};
+            const actual = {};
 
             return session
                 .executeSql('SELECT 1')
@@ -87,7 +66,18 @@ describe('@integration relational miscellaneous tests', () => {
                         actual.meta = meta;
                     }
                 })
-                .then(() => expect(actual).to.deep.equal(expected));
+                .then(() => {
+                    expect(actual).to.have.all.keys('meta', 'row');
+                    expect(actual.meta).to.have.lengthOf(1);
+                    expect(actual.meta[0]).to.be.an.instanceOf(Column);
+                    expect(actual.meta[0].getType()).to.equal(1);
+                    expect(actual.meta[0].getColumnLabel()).to.equal('1');
+                    expect(actual.meta[0].getColumnName()).to.equal('');
+                    expect(actual.meta[0].getTableLabel()).to.equal('');
+                    expect(actual.meta[0].getTableName()).to.equal('');
+                    expect(actual.meta[0].getSchemaName()).to.equal('');
+                    expect(actual.meta[0].getLength()).to.equal(1);
+                });
         });
     });
 });
