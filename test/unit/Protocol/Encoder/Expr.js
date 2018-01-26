5a071a3d0c33b9f8ecd192625748e7a7616ac69e
Rui Quelhas <rui.quelhas@oracle.com>
2018-01-26 18:01:28 +0000

    BUG#27177864: Skip collection.add() native field parsing

---
+++ test/unit/Protocol/Encoder/Expr.js
@@ -0,0 +1,92 @@
+'use strict';
+
+/* eslint-env node, mocha */
+
+// npm `test` script was updated to use NODE_PATH=.
+const Expr = require('lib/Protocol/Encoder/Expr');
+const ExprStub = require('lib/Protocol/Stubs/mysqlx_expr_pb').Expr;
+const Scalar = require('lib/Protocol/Stubs/mysqlx_datatypes_pb').Scalar;
+const Type = require('lib/Protocol/Stubs/mysqlx_expr_pb').Expr.Type;
+const expect = require('chai').expect;
+const proxyquire = require('proxyquire');
+const td = require('testdouble');
+
+describe('Encoder', () => {
+    context('Expr', () => {
+        let FakeExpr, parse;
+
+        beforeEach('setup fakes', () => {
+            parse = td.function();
+
+            FakeExpr = proxyquire('lib/Protocol/Encoder/Expr', { '../../ExprParser': { parse } });
+        });
+
+        afterEach('reset fakes', () => {
+            td.reset();
+        });
+
+        context('encodeExpr()', () => {
+            beforeEach('setup fakes', () => {
+                td.when(parse(), { ignoreExtraArgs: true }).thenReturn({ output: {} });
+            });
+
+            it('should return the input value if it is an expression', () => {
+                const literal = new Scalar();
+                literal.setType(Scalar.Type.V_NULL);
+
+                const expected = new ExprStub();
+                expected.setType(Type.LITERAL);
+                expected.setLiteral();
+
+                const actual = FakeExpr.encodeExpr(expected);
+
+                expect(td.explain(parse).callCount).to.equal(0);
+                expect(actual.toObject()).to.deep.equal(expected.toObject());
+            });
+
+            it('should enable expression parsing by default', () => {
+                FakeExpr.encodeExpr('foo');
+
+                expect(td.explain(parse).callCount).to.equal(1);
+                expect(td.explain(parse).calls[0].args[0]).to.equal('foo');
+            });
+
+            it('should stringify non-string input for parsing', () => {
+                FakeExpr.encodeExpr({ foo: 'bar' });
+
+                expect(td.explain(parse).callCount).to.equal(1);
+                expect(td.explain(parse).calls[0].args[0]).to.equal('{"foo":"bar"}');
+            });
+
+            it('should disable expression parsing on demand', () => {
+                FakeExpr.encodeExpr({ foo: 'bar' }, { parse: false });
+
+                expect(td.explain(parse).callCount).to.equal(0);
+            });
+
+            it('should encode a JavaScript array', () => {
+                const array1 = Expr.encodeExpr(['foo', 'bar']);
+                const array2 = Expr.encodeExpr(['foo', 'bar'], { parse: false });
+
+                expect(array1.getType()).to.equal(Type.ARRAY);
+                expect(array2.getType()).to.equal(Type.ARRAY);
+            });
+
+            it('should encode a JavaScript plain object', () => {
+                const obj1 = Expr.encodeExpr({ foo: 'bar' });
+                const obj2 = Expr.encodeExpr({ foo: 'bar' }, { parse: false });
+
+                expect(obj1.getType()).to.equal(Type.OBJECT);
+                expect(obj2.getType()).to.equal(Type.OBJECT);
+            });
+
+            it('should encode a JavaScript primitive type', () => {
+                const id = Expr.encodeExpr('foo');
+                const literal = Expr.encodeExpr('foo', { parse: false });
+
+                expect(id.getType()).to.equal(Type.IDENT);
+                expect(literal.getType()).to.equal(Type.LITERAL);
+            });
+        });
+    });
+});
